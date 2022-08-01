3204548e028aec2e7efbe748d0d354296f8c3d8e
Ian Hsieh <ihsiehsieh@gmail.com>
2022-08-01 15:02:13 +0200

    hmmm

---
+++ Walk before you run.R
@@ -0,0 +1,13 @@
+#---
+#title: "Walk before you run"
+#author: "Ian Hsieh"
+#date: "2022-08-01"
+#output: github_document
+#---
+
+#
+#```{r, eval=TRUE}
+#+ r setup, include = FALSE
+ 2+2 -1
+#```
+
